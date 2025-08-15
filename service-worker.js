82876a6d6b110953503aeb3a3153a827fea34fa4
muhammadkabir153-ai <muhammadkabir153@gmail.com>
2025-08-15 22:57:33 +0100

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,23 @@
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open("sales-app-cache").then(cache => {
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open("sales-app-v1").then((cache) => {
       return cache.addAll([
-        "./",
-        "./index.html",
-        "./style.css",
-        "./app.js",
-        "./manifest.json"
+        "/",
+        "/index.html",
+        "/style.css",
+        "/app.js",
+        "/manifest.json",
+        "/icon-192.png",
+        "/icon-512.png"
       ]);
     })
   );
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
     })
   );
 });
